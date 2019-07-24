037f68bfbfe88007e8298bcb8fbd4184912b3b15
Dru Vagale <dru@the4d.ca>
2019-07-24 17:06:33 -0400

    removed all spaces

--- dyndns-do.sh
+++ dyndns-do.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-
 api_host="https://api.digitalocean.com/v2"
-
 #sleep interval is set to 6hrs (default)
 sleep_interval=${SLEEP_INTERVAL:-21600}
-
 test -z $DIGITALOCEAN_TOKEN && die "DIGITALOCEAN_TOKEN not set!"
 test -z $DOMAIN && die "DOMAIN not set!"
 test -z $NAME && die "NAME not set!"
-
 dns_list="$api_host/domains/$DOMAIN/records"
-
 while :
 do
     domain_records=$(curl -s -X GET \
@@ -19,17 +14,13 @@ do
         $dns_list)
     record_id=$(echo $domain_records| jq ".domain_records[] | select(.type == \"A\" and .name == \"$NAME\") | .id")
     record_data=$(echo $domain_records| jq -r ".domain_records[] | select(.type == \"A\" and .name == \"$NAME\") | .data")
-    
     test -z $record_id && die "No record found with given domain name!"
-
     ip="$(curl -s ipinfo.io/ip)"
     data="{\"type\": \"A\", \"name\": \"$NAME\", \"data\": \"$ip\"}"
     url="$dns_list/$record_id"
-
     if [[ -n $ip ]]; then
         if [[ "$ip" != "$record_data" ]]; then
             echo "existing DNS record address ($record_data) doesn't match current IP ($ip), sending data=$data to url=$url"
-
             curl -s -X PUT \
                 -H "Content-Type: application/json" \
                 -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" \
@@ -39,8 +30,6 @@ do
     else
         echo "IP wasn't retrieved within allowed interval. Will try $sleep_interval seconds later.."
     fi
-
     sleep $sleep_interval
 done
-
 exit 0
